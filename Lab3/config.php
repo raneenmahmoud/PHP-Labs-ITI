a23e4b09bbb997382fea1ebda3214966680675d3
raneenmahmoud <90557756+raneenmahmoud@users.noreply.github.com>
2023-03-15 20:33:13 +0200

    Delete Lab3 directory

--- Lab3/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("_counter_file_","counter.txt");
-define("_session_key_counter_","is_counted");
--- Lab3/views/counter.php
+++
@@ -1,5 +0,0 @@
-<?php
-$counter = new Counter();
-$counter->increament_and_update();
-$count = $counter->get_count();
-echo "<h1> Count = $count </h1>";
